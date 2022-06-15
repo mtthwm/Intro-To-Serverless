1d3dc75358df224d4f5e77d3ec6bcd27ded1a9e4
Matthew Morales <mtthwmrls9@gmail.com>
2022-06-14 21:43:40 -0600

    Modified the password function

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -1,6 +1,8 @@
 module.exports = async function (context, req) {
+    const expectedPassword = "letmein";
     const password = req.query.password;
-    const responseMessage = password;
+
+    const responseMessage = password === expectedPassword ? "Access Granted" : "Access Denied";
 
     context.res = {
         status: password ? 200 : 400,
