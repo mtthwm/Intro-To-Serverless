64b06bf44ceb42a3c6327aefe6e012aeb88e4189
Matthew Morales <mtthwmrls9@gmail.com>
2022-06-15 19:19:20 -0600

    Working on the twocatz api

--- twocatz/index.js
+++ twocatz/index.js
@@ -1,19 +1,44 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    const url = (text) => {
-        return `https://cataas.com/cat/says/${text}`;
-    };
-
+    
     const axios = require('axios');
 
-    const response = await axios.get(url("Serverless"));
+    const catCount = 2;
 
-    const image = Buffer.from(response.data, 'binary').toString('base64');
+    const getUrl = (text) => {
+        return `https://cataas.com/cat/says/${text}`;
+    };
+    const getName = () => {
+        const names = [
+            "Shreya", 
+            "Emily", 
+            "Fifi", 
+            "Beau", 
+            "Evelyn", 
+            "Julia", 
+            "Daniel", 
+            "Fardeen"
+        ];
+
+        return names[Math.floor(Math.random() * names.length)];
+    }
+
+    const finalObject = {};
+    const names = [];
+
+    for (let i = 0; i < catCount; i++)
+    {
+        const name = getName();
+        const response = await axios.get(getUrl(name));
+        const image = Buffer.from(response.data, 'binary').toString('base64');
+        finalObject[`cat${i+1}`] = image;
+        names.push(name);
+    }
+
+    finalObject[`names`] = names;
 
     context.res = {
         status: 200,
-        body: {
-            image,
-        }
+        body: finalObject,
     }
 }
