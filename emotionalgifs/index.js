b9448c82e02ac030551959f0e82dedd70ff1fe53
Matthew Morales <mtthwmrls9@gmail.com>
2022-06-23 20:05:43 -0600

    Added image emotion endpoint

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,4 +1,5 @@
 const multipart = require('parse-multipart');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     const boundary = multipart.getBoundary(req.headers['content-type']);
@@ -9,8 +10,30 @@ module.exports = async function (context, req) {
 
     const base64Image = parts[0].data.toString('base64');
 
+    const emotions = getImageEmotions(parts[0].data);
+
+
     context.res = {
         status: 200,
-        body: base64Image,
+        body: emotions,
     };
 }
+
+const getImageEmotions = (imageBuffer) => {
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const urlBase = process.env.ENDPOINT + '/face/v1.0/detect';
+    const parameters = new URLSearchParams({
+        'returnFaceAttributes': 'emotion',
+        'returnFaceIds': 'true',
+    });
+
+    const response = await fetch(`${urlBase}?${parameters.toString()}`, {
+        method: 'POST',
+        body: imageBuffer,
+        headers: {
+            'Content-Type': 'application/octet-stream',
+        },
+    });
+
+    return response.json();
+};
